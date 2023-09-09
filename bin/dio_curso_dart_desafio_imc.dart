4c6ea57d9faebab8b886793d269d8ea7588c32d6
Ronaldo Rocha <ronaldocuca@gmail.com>
2023-09-08 21:40:27 -0300

    init

---
+++ bin/dio_curso_dart_desafio_imc.dart
@@ -0,0 +1,5 @@
+import 'package:dio_curso_dart_desafio_imc/dio_curso_dart_desafio_imc.dart' as dio_curso_dart_desafio_imc;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dio_curso_dart_desafio_imc.calculate()}!');
+}
---
+++ test/dio_curso_dart_desafio_imc_test.dart
@@ -0,0 +1,8 @@
+import 'package:dio_curso_dart_desafio_imc/dio_curso_dart_desafio_imc.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
